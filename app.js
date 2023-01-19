dc7936227ebe22f22de78b4ca4c2554fe85069c9
unknown <glorenzo2001@gmail.com>
2023-01-19 15:30:36 +0000

    Added animation

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const circle = document.querySelector("circle");
 const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
 
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart() {
         console.log('Timer started');
     },
     onTick() {
-        console.log('Timer ticked down');
+        circle.setAttribute('stroke-dashoffset', currentOffset);
+        currentOffset = currentOffset - 50;
     },
     onPause() {
         console.log('Timer paused');
